a2220936a32b4d7e2e52925c45dedecf7bffb5a0
David B. Bitton <david@codenoevil.com>
2013-04-20 03:07:04 -0400

    works now

--- wordpress/recipes/appsetup.rb
+++ wordpress/recipes/appsetup.rb
@@ -1,41 +1,57 @@
-# require 'net/http'
+require 'uri'
+require 'net/http'
+require 'net/https'
 
-# uri = URI('https://api.wordpress.org/secret-key/1.1/salt/')
-# keys = Net::HTTP.get(uri)
-# notifies :write, "log[Keys: ${keys}]"
+uri = URI.parse("https://api.wordpress.org/secret-key/1.1/salt/")
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+request = Net::HTTP::Get.new(uri.request_uri)
+response = http.request(request)
+keys = response.body
 
 node[:deploy].each do |app_name, deploy|
 
-# 	script "install_composer" do
-# 		interpreter "bash"
-# 		user "root"
-# 		cwd "#{deploy[:deploy_to]}/current"
-# 		code <<-EOH
-# 		curl -s https://getcomposer.org/installer | php
-# 		php composer.phar install
-# 		EOH
-# 	end
-  
-# 	template "#{deploy[:deploy_to]}/current/wp-config.php" do
-# 		source "wp-config.php.erb"
-# 		mode 0660
-# 		group deploy[:group]
+        template "#{deploy[:deploy_to]}/current/composer.json" do
+                source "composer.json.erb"
+                mode 0660
+                group deploy[:group]
 
-# 		if platform?("ubuntu")
-# 		  owner "www-data"
-# 		elsif platform?("amazon")   
-# 		  owner "apache"
-# 		end
+                if platform?("ubuntu")
+                  owner "www-data"
+                elsif platform?("amazon")
+                  owner "apache"
+                end
+        end
 
-# 		variables(
-# 			:database        => (deploy[:database][:database] rescue nil),
-# 			:user            => (deploy[:database][:username] rescue nil),
-# 			:password        => (deploy[:database][:password] rescue nil),
-# 			:host			 => (deploy[:database][:host] rescue nil),
-# 			:keys        	 => (keys rescue nil)
-# 		)
-# 	end		  
-	  
+        script "install_composer" do
+                interpreter "bash"
+                user "root"
+                cwd "#{deploy[:deploy_to]}/current"
+                code <<-EOH
+                curl -s https://getcomposer.org/installer | php
+                php composer.phar install
+                EOH
+        end
+
+        template "#{deploy[:deploy_to]}/current/wp-config.php" do
+                source "wp-config.php.erb"
+                mode 0660
+                group deploy[:group]
+
+                if platform?("ubuntu")
+                  owner "www-data"
+                elsif platform?("amazon")
+                  owner "apache"
+                end
+
+                variables(
+                        :database        => (deploy[:database][:database] rescue nil),
+                        :user            => (deploy[:database][:username] rescue nil),
+                        :password        => (deploy[:database][:password] rescue nil),
+                        :host            => (deploy[:database][:host] rescue nil),
+                        :keys            => (keys rescue nil)
+                )
+        end
+
 end
-
-# notifies :write, "log[Navigate to 'http://#{node[:opsworks][:layers][:lb][:instances].first[:public_dns_name]}/wp-admin/install.php' to complete wordpress installation]"
