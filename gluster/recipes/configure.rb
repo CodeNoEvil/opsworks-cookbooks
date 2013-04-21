798f0801dc6b162666cd6f3787ccd81022e0659a
David B. Bitton <david@codenoevil.com>
2013-04-21 14:19:54 -0400

    implemented opsworks

--- gluster/recipes/configure.rb
+++ gluster/recipes/configure.rb
@@ -1,17 +1,26 @@
+# gluster server configure recipe
+# copyright 2013 Code No Evil, LLC
 
+aws_instance_id         = node[:opsworks][:instance][:aws_instance_id]
+layer                   = node[:opsworks][:instance][:layers].first
+hostname                = node[:opsworks][:instance][:hostname]
+instances               = node[:opsworks][:layers].fetch(layer)[:instances]
+is_first_node           = instances.keys.index(hostname) == 0
 
-
-aws_instance_id = node[:opsworks][:instance][:instance_id]
-layer = node[:opsworks][:instance][:layer].first
-hostname = node[:opsworks][:instance][:hostname]
-is_first_node = node[:opsworks][:layers][layer][:instances].index(hostname) == 0
+# print "AWS Instance ID: #{aws_instance_id}\n"
+# print "Layer: #{layer}\n"
+# print "Keys: #{instances.keys}\n"
+# print "Is First: #{is_first_node}\n"
+# print "Hostname: #{hostname}\n"
 
 if is_first_node then
-	node[:opsworks][:layers][layer][:instances].each do |instance|
-
-		execute "gluster peer probe #{instance[:private_ip]}" do
-			not_if "gluster peer status | grep '^Hostname: #{peer}'" 
-			not_if { instance[:private_ip] }
-		end
-	end
+        instances.each_value do |instance|
+                private_dns_name = instance[:private_dns_name]
+# print "Private IP: #{private_dns_name}\n"
+                execute "peer probing" do
+                        command "echo 'Probing #{private_dns_name}'; gluster peer probe #{private_dns_name}"
+                        not_if "gluster peer status | grep '^Hostname: #{private_dns_name}'"
+                        not_if { instance[:aws_instance_id] == aws_instance_id }
+                end
+        end
 end
