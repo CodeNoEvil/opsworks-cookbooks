85503ad78560c7f953f43b1d4d50633fcf799835
David B. Bitton <david@codenoevil.com>
2013-04-20 22:08:47 -0400

    getting there

---
+++ gluster/recipes/configure.rb
@@ -0,0 +1,17 @@
+
+
+
+aws_instance_id = node[:opsworks][:instance][:instance_id]
+layer = node[:opsworks][:instance][:layer].first
+hostname = node[:opsworks][:instance][:hostname]
+is_first_node = node[:opsworks][:layers][layer][:instances].index(hostname) == 0
+
+if is_first_node then
+	node[:opsworks][:layers][layer][:instances].each do |instance|
+
+		execute "gluster peer probe #{instance[:private_ip]}" do
+			not_if "gluster peer status | grep '^Hostname: #{peer}'" 
+			not_if { peer == node['ipaddress'] }
+		end
+	end
+end
