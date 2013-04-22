cd2e6a588a752b05d5e2efc985de933f2bf02408
David Bitton <david@codenoevil.com>
2013-04-22 11:15:28 -0400

    dealt with results of sort_by
    sort_by creates arrays of arrays for k/v hash pairs

--- gluster/recipes/configure.rb
+++ gluster/recipes/configure.rb
@@ -4,26 +4,27 @@
 aws_instance_id         = node[:opsworks][:instance][:aws_instance_id]
 layer                   = node[:opsworks][:instance][:layers].first
 hostname                = node[:opsworks][:instance][:hostname]
-instances               = node[:opsworks][:layers].fetch(layer)[:instances].sort_by{ |k,v| v[:booted_at]}
-is_first_node           = instances.keys.index(hostname) == 0
+instances               = node[:opsworks][:layers].fetch(layer)[:instances].sort_by{|k,v| v[:booted_at] }
+is_first_node           = instances.index{|i|i[0] == hostname} == 0
 
 Chef::Log.debug("aws_instance_id: #{aws_instance_id}")
 Chef::Log.debug("layer: #{layer}")
-Chef::Log.debug("instances: #{instances.keys.join(',')}")
+Chef::Log.debug("instances: #{instances.each{|i| i[0] }.join(',')}")
 Chef::Log.debug("is_first_node: #{is_first_node}")
 Chef::Log.debug("hostname: #{hostname}")
 
 if is_first_node then
-	Chef::Log.info("First Node; Probing peers")
+        Chef::Log.info("First Node; Probing peers")
 
-    instances.each_value do |instance|
-            private_dns_name = instance[:private_dns_name]
-			
-			Chef::Log.debug("Peer private_dns_name: #{private_dns_name}")
-            
-            execute "gluster peer probe #{private_dns_name}" do
-                    not_if "gluster peer status | grep '^Hostname: #{private_dns_name}'"
-                    not_if { instance[:aws_instance_id] == aws_instance_id }
-            end
+    instances.each do |i|
+        instance = i[1]
+        private_dns_name = instance[:private_dns_name]
+
+        Chef::Log.debug("Peer private_dns_name: #{private_dns_name}")
+
+        execute "gluster peer probe #{private_dns_name}" do
+                not_if "gluster peer status | grep '^Hostname: #{private_dns_name}'"
+                not_if { instance[:aws_instance_id] == aws_instance_id }
+        end
     end
 end
